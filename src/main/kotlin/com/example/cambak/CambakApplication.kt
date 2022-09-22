59cc9f9da81aa94f8ed0605650d4aea02d187154
201311113 <antjdlekd123@naver.com>
2022-09-22 16:21:08 +0900

    init

---
+++ src/main/kotlin/com/example/cambak/CambakApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cambak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CambakApplication
+
+fun main(args: Array<String>) {
+	runApplication<CambakApplication>(*args)
+}
